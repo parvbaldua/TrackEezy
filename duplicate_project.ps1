a40c1854340faee78a40fed6f9989b1475c65edc
Parv Baldua <parvbaldua@gmail.com>
2026-02-14 11:55:48 +0530

    Cleanup: removed temp files and updated gitignore

--- duplicate_project.ps1
+++
@@ -1,5 +0,0 @@
-$source = Get-Location
-$dest = "$((Get-Item .).Parent.FullName)\ginto"
-Write-Host "Copying from $source to $dest"
-Copy-Item -Path $source -Destination $dest -Recurse -Force
-Write-Host "Copy complete"
